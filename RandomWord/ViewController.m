ec9a6c1deeee6b425a8b7e8284eeee1280d9f882
Juhnowski <juhnowski@gmail.com>
2018-05-01 22:00:15 +0300

    initial

--- RandomWord/ViewController.m
+++ RandomWord/ViewController.m
@@ -26,4 +26,29 @@
 }
 
 
+- (IBAction)RandomWord:(id)sender {
+    int RandomWord = arc4random() % 5;
+    switch (RandomWord) {
+        case 0:
+            self.Label.text = @"Red";
+            break;
+        case 1:
+            self.Label.text = @"Yellow";
+            break;
+        case 2:
+            self.Label.text = @"Blue";
+            break;
+        case 3:
+            self.Label.text = @"Green";
+            break;
+        case 4:
+            self.Label.text = @"Black";
+            break;
+        case 5:
+            self.Label.text = @"White";
+            break;
+        default:
+            break;
+    }
+}
 @end
